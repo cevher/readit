005117bf40d0893b79ad2572ea5b3e0195ef22c4
cevher <efeozden@gmail.com>
2017-02-09 20:05:36 +0200

    After stylesheet and linking pages

--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
+from django.db.models import Count
 from django.shortcuts import render
-from.models import book
+from.models import book, Author
+from django.views.generic import View, DetailView
 # Create your views here.
+
 
 def list_books(request):
     """
@@ -11,3 +14,21 @@ def list_books(request):
         'books' : books,
     }
     return render(request, "list.html",context)
+
+class AuthorList(View):
+    def get(self, request):
+        authors = Author.objects.annotate(
+        published_books=Count('books')).filter(
+        published_books__gt=0)
+        context ={
+            'authors':authors,
+        }
+        return render(request, "authors.html", context)
+
+class BookDetail(DetailView):
+    model = book
+    template_name = 'book.html'
+
+class AuthorDetail(DetailView):
+    model=Author
+    template_name='author.html'
