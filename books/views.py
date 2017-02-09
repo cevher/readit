75f5e2a0c736104180b9487c7ade0aa7cf266c6a
cevher <efeozden@gmail.com>
2017-02-09 16:29:17 +0200

    Create queryand templates

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from.models import book
 # Create your views here.
+
+def list_books(request):
+    """
+    List the books that have reviews
+    """
+    books = book.objects.exclude(date_reviewed__isnull=True).prefetch_related('authors')
+    context={
+        'books' : books,
+    }
+    return render(request, "list.html",context)
